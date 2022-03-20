acba5a44093f9cc1ca5e75ddc4571b2f0045ce7f
“jamelscott” <itsjamelscott@gmail.com>
2022-03-19 20:24:18 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import {Link} from 'react-router-dom'
 
 function App() {
+
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+
+      axios.get(`http://localhost:8000/blog`)
+      .then((response) => {
+        console.log(response)
+        setBlogs(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }, []);
+
+
+  // const allBlogs = blogs.map((element, idx) => {
+  //   return (<>
+    
+  //   <p>{element.name}</p>
+  // <p>{element.content}</p>
+  //   </>
+
+  //   )
+  // });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Router>
+    <Routes>
+      <Route
+      exact
+      path={'/'}
+      >
+
+      </Route>
+    </Routes>
+  </Router>
+    <h1>Welcome to Blog land</h1>
+    <Link to={'/'}>View all blogs</Link>
+    <Link to={'/'}>Create a new blog</Link>
+
+  </>
   );
 }
 
---
+++ src/components/pages/home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+    return ( 
+        <h1>Welcome to Blogland</h1>
+     );
+}
+
+export default Home;
